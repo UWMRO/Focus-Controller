ef1664f7d8d0cef3434a17184fa386e5c8083fe6
dmyerc <112904335+dmyerc@users.noreply.github.com>
2025-05-05 23:58:54 -0700

    Add files via upload

--- focusController.ino
+++ focusController.ino
@@ -1,27 +1,30 @@
 /*
-  Web Server
+  MRO Focus Controller 
 
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
+  An arduino that sends focussing based commands to the Manastash Ridge Observatory
+  
+  The arduino hosts a simple Arduino web server, and recieves commands via
+  web server actions sent over it's ethernet port
 
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
+ * Clock Motor Output - pin 9
+ * Direction Motor Output  - pin 8
+ * Enable Motor Output - pin 7
+ * Limit pin 1 - pin 6
+ * Limit pin 2 - pin 5
+ * Potentiometer - TinkerKit connection "IN3"
 
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
- 
+ Code written by AUEG Arduino Team 2025! 
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <Thread.h>
+#include <ThreadController.h>
+#include <StaticThreadController.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
+// MAC address and IP address for controller
 byte mac[] = {
   0xA8, 0x61, 0x0A, 0xAE, 0x25, 0x13
 };
@@ -32,7 +35,10 @@ IPAddress ip(192, 168, 1, 11);
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
-// Focuser state
+// Multithread for handling motor movement in background
+Thread moveMotor = Thread();
+
+// Focuser fields
 volatile int position = 0;
 bool moving = false;
 int target_steps = 0;
@@ -41,21 +47,21 @@ const int stepDelay = 100; // ms per step
 const int up_limit = 100;
 const int down_limit = -100;
 
-void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+int moveSteps = 0;
+float clkFreq = 50; // in Hz
+bool abortMovement = false;
 
+int clkPin = 9;
+int dirPin = 8;
+int enPin = 7;
+
+void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.println("Ethernet WebServer Example");
+  Serial.println("Ethernet WebServer Booting");
 
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
@@ -73,8 +79,12 @@ void setup() {
 
   // start the server
   server.begin();
-  Serial.print("server is at ");
+  Serial.print("Server starting at ");
   Serial.println(Ethernet.localIP());
+
+  //initialize thread
+  moveMotor.onRun(startMovement);
+  moveMotor.enabled = true;
 }
 
 
@@ -85,57 +95,22 @@ void loop() {
   if (client) {
 
     handleClient(client);
-    // Serial.println("new client");
-    // // an http request ends with a blank line
-    // boolean currentLineIsBlank = true;
-    // while (client.connected()) {
-    //   if (client.available()) {
-    //     char c = client.read();
-    //     Serial.write(c);
-    //     // if you've gotten to the end of the line (received a newline
-    //     // character) and the line is blank, the http request has ended,
-    //     // so you can send a reply
-    //     if (c == '\n' && currentLineIsBlank) {
-    //       // send a standard http response header
-    //       client.println("HTTP/1.1 200 OK");
-    //       client.println("Content-Type: text/html");
-    //       client.println("Connection: close");  // the connection will be closed after completion of the response
-    //       client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-    //       client.println();
-    //       client.println("<!DOCTYPE HTML>");
-    //       client.println("<html>");
-    //       // output the value of each analog input pin
-    //       for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-    //         int sensorReading = analogRead(analogChannel);
-    //         client.print("analog input ");
-    //         client.print(analogChannel);
-    //         client.print(" is ");
-    //         client.print(sensorReading);
-    //         client.println("<br />");
-    //       }
-    //       client.println("</html>");
-    //       break;
-    //     }
-    //     if (c == '\n') {
-    //       // you're starting a new line
-    //       currentLineIsBlank = true;
-    //     } else if (c != '\r') {
-    //       // you've gotten a character on the current line
-    //       currentLineIsBlank = false;
-    //     }
-    //   }
-    }
+
     // give the web browser time to receive the data
     delay(1);
+
     // close the connection:
     client.stop();
     //Serial.println("client disconnected");
   }
+}
 
-  void handleClient(EthernetClient client) {
+
+void handleClient(EthernetClient client) {
     char request[128];
     int i = 0;
-  
+
+    // Retrieve client command
     while (client.connected() && i < 127) {
       if (client.available()) {
         char c = client.read();
@@ -145,35 +120,71 @@ void loop() {
     }
     request[i] = '\0';
 
-  // To access: 192.168.1.11/status
-  // or curl -X GET http://192.168.1.11/status in terminal
+
+  /*
+    To test the following commands enter something like "192.168.1.11/status" in web browser
+    can also test with "curl -X GET http://192.168.1.11/status" in terminal like powershell
+    ^ Make sure ethernet port IP of test computer is configured 
+    (E.g. access ethernet settings of computer, change from DHCP to manual, use ip like 192.168.1.9 and basic mask like 255.255.0.0 )
+  */
+
+  /*
+  Command: move?steps
+  Paramters: steps: <int32>
+  Returns: {code: <int32>}
+            ^ Descriptions of each json field are described in Focuser Arduino Specification Google Doc
+  Test code: "curl -X POST http://192.168.1.11/move?steps=500"
+  */
+
+
+  if (strstr(request, "POST /move?steps")) {
+    char* stepsParam = strstr(request, "steps=");
+    if (stepsParam && moveMotor.shouldRun()) {
+      moveSteps = atoi(stepsParam + 6);
+      Serial.print("moving ");
+      Serial.println(moveSteps);
+
+      moveMotor.run();
+    }
+    sendJSONResponse(client, 200, "\"code\":200");
+  }
+  /*
+  Command: status
+  Paramters: None
+  Returns: {moving: <bool>, voltage: <int32>, upper_limit: <bool>, lower_limit: <bool>, step: <int32>}
+            ^ Descriptions of each json field are described in Focuser Arduino Specification Google Doc
+  Test code: "curl -X GET http://192.168.1.11/status"
+  */
   if (strstr(request, "GET /status")) {
     sendJSONResponse(client, 200, 
       "\"moving\":" + String(moving) + 
       ",\"step\":" + String(position) + 
       ",\"limit\":" + String(checkLimits()));
+
+    Serial.println("Status Message Sent! :D");
   }
 
-  // curl -X POST http://192.168.1.11/move?steps=50
-  else if (strstr(request, "POST /move?steps")) {
-    char* stepsParam = strstr(request, "steps=");
-    if (stepsParam) {
-      int steps = atoi(stepsParam + 6);
-      Serial.print("moving ");
-      Serial.println(steps);
-      startMovement(steps);
-    }
-    sendJSONResponse(client, 200, "\"code\":200");
-  }
-
-  // curl -X POST http://192.168.1.11/abort
+  /*
+  Command: abort
+  Paramters: none
+  Returns: {code: <int32>}
+            ^ Descriptions of each json field are described in Focuser Arduino Specification Google Doc
+  Test code: "curl -X POST http://192.168.1.11/abort"
+  */
   else if (strstr(request, "POST /abort")) {
-    moving = false;
+    if (moving) {
+      abortMovement = true;
+      Serial.println("Aborting movement");
+    }
+    else {
+      Serial.println("No Movement to Abort");
+    }
+
     sendJSONResponse(client, 300, "\"code\":300");
-    Serial.println("Aborting movement");
   }
 }
 
+// Code for formatting json return strings
 void sendJSONResponse(EthernetClient client, int code, String body) {
   client.println("HTTP/1.1 " + String(code) + " OK");
   client.println("Content-Type: application/json");
@@ -182,6 +193,7 @@ void sendJSONResponse(EthernetClient client, int code, String body) {
   client.println("{" + body + "}");
 }
 
+// Emulating the potentiometer position
 void updatePosition() {
   if (moving && (millis() - lastStepTime > stepDelay)) {
     position += (target_steps > 0) ? 1 : -1;
@@ -199,8 +211,37 @@ bool checkLimits() {
   return (position >= up_limit) || (position <= down_limit);
 }
 
-void startMovement(int steps) {
-  target_steps = steps;
+void startMovement() {
+  
+  //Disable thread so another movement call won't overwrite the current movement
+  moveMotor.enabled = false;
   moving = true;
+
+  // Period is 1/f, we want to convert to ms then divide by 2 since pulseTime should be half of a clock cycle
+  long pulseTime = long(float(1)*500000.0/clkFreq);
+
+  int i = 0;
+  while (i < moveSteps && !abortMovement) {
+    long pastTime = micros();
+    digitalWrite(clkPin, HIGH);
+    Serial.print("clkHigh");
+
+    // delay until halfway through period designated by clk_frequency
+    while (micros() - pastTime < pulseTime) {}
+    
+    pastTime = micros();
+    digitalWrite(clkPin, LOW);
+    Serial.print("clkLow");
+
+    // delay until reach the end of period
+    while (micros() - pastTime < pulseTime) {}
+
+    i++;
+  }
+  Serial.println();
+  Serial.println("Movement Finished");
+
+  moving = false;
+  moveMotor.enabled = true;
 }
 
